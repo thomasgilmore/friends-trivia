bba922674d618435a40ec0d6515a3b92baaa4e44
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-07-30 19:14:33 -0400

    added more questions

--- srcipt.js
+++ srcipt.js
@@ -3,16 +3,16 @@ let numberOfWrongAnswers = 0;
 let score = 0;
 
 const wrongAnswer = (classOfButtonClicked, classOfOtherButton) => {
-    console.log('wrong');
     $(classOfButtonClicked).css('background-color', '#FF0000');
+    $(classOfButtonClicked).attr("disabled","disabled");
     $(classOfOtherButton).attr("disabled","disabled");
     $(classOfOtherButton).removeClass("hover");
     numberOfWrongAnswers++;
     console.log(numberOfWrongAnswers);
 };
 const rightAnswer = (classOfButtonClicked, classOfOtherButton) => {
-    console.log('right');
     $(classOfButtonClicked).css('background-color', '#00FF0A');
+    $(classOfButtonClicked).attr("disabled","disabled");
     $(classOfOtherButton).attr("disabled","disabled");
     $(classOfOtherButton).removeClass("hover");
     numberOfCorrectAnswers++;
@@ -30,7 +30,17 @@ $('.A-city-bus').on('click', function() { wrongAnswer('.A-city-bus', '.A-job-tit
 $('.A-job-title').on('click', function() { rightAnswer('.A-job-title', '.A-city-bus'); });
 $('.Cole-Sprouse').on('click', function() { rightAnswer('.Cole-Sprouse', '.Dylan-Sprouse'); });
 $('.Dylan-Sprouse').on('click', function() { wrongAnswer('.Dylan-Sprouse', '.Cole-Sprouse'); });
+$('.New-York-City').on('click', function() { rightAnswer('.New-York-City', '.Los-Angeles'); });
+$('.Los-Angeles').on('click', function() { wrongAnswer('.Los-Angeles', '.New-York-City'); });
+$('.Ill-Be-There-For-Them').on('click', function() { wrongAnswer('.Ill-Be-There-For-Them', '.Ill-Be-There-For-You'); });
+$('.Ill-Be-There-For-You').on('click', function() { rightAnswer('.Ill-Be-There-For-You', '.Ill-Be-There-For-Them'); });
+$('.St-James-Perk').on('click', function() { wrongAnswer('.St-James-Perk', '.Central-Perk'); });
+$('.Central-Perk').on('click', function() { rightAnswer('.Central-Perk', '.St-James-Perk'); });
+$('.A-yellow-picture-frame').on('click', function() { rightAnswer('.A-yellow-picture-frame', '.A-coat-hook'); });
+$('.A-coat-hook').on('click', function() { wrongAnswer('.A-coat-hook', '.A-yellow-picture-frame'); });
+$('.Cynthia-Fee').on('click', function() { wrongAnswer('.Cynthia-Fee', '.The-Rembrandts'); });
+$('.The-Rembrandts').on('click', function() { rightAnswer('.The-Rembrandts', '.Cynthia-Fee'); });
 
-if (numberOfCorrectAnswers + numberOfWrongAnswers === 5) {
+if (numberOfCorrectAnswers + numberOfWrongAnswers === 10) {
     score = (numberOfCorrectAnswers / 5) * 100;
 }
