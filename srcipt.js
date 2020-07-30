ceb57585a1c69ad38f0cc24ad970f957a1362b05
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-07-30 13:33:03 -0400

    javascript funtion is also keeping players score now

--- srcipt.js
+++ srcipt.js
@@ -1,14 +1,23 @@
+let numberOfCorrectAnswers = 0;
+let numberOfWrongAnswers = 0;
+let score = 0;
+
 const wrongAnswer = (classOfButtonClicked, classOfOtherButton) => {
     console.log('wrong');
     $(classOfButtonClicked).css('background-color', '#FF0000');
     $(classOfOtherButton).attr("disabled","disabled");
     $(classOfOtherButton).removeClass("hover");
+    numberOfWrongAnswers++;
+    console.log(numberOfWrongAnswers);
 };
 const rightAnswer = (classOfButtonClicked, classOfOtherButton) => {
     console.log('right');
     $(classOfButtonClicked).css('background-color', '#00FF0A');
     $(classOfOtherButton).attr("disabled","disabled");
     $(classOfOtherButton).removeClass("hover");
+    numberOfCorrectAnswers++;
+    $('.score').html(numberOfCorrectAnswers.toString());
+    console.log(numberOfCorrectAnswers);
 };
 
 $('.Days-of-Our-Lives').on('click', function() { rightAnswer('.Days-of-Our-Lives', '.General-Hospital'); });
@@ -21,3 +30,7 @@ $('.A-city-bus').on('click', function() { wrongAnswer('.A-city-bus', '.A-job-tit
 $('.A-job-title').on('click', function() { rightAnswer('.A-job-title', '.A-city-bus'); });
 $('.Cole-Sprouse').on('click', function() { rightAnswer('.Cole-Sprouse', '.Dylan-Sprouse'); });
 $('.Dylan-Sprouse').on('click', function() { wrongAnswer('.Dylan-Sprouse', '.Cole-Sprouse'); });
+
+if (numberOfCorrectAnswers + numberOfWrongAnswers === 5) {
+    score = (numberOfCorrectAnswers / 5) * 100;
+}
